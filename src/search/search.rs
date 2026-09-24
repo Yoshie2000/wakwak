f59e408c606409c56bae8ba8187761a234fe6896
Yoshie2000 <patrick.leonhardt@gmx.net>
2026-09-24 21:20:12 +0200

    Bench: 4641366

--- src/search/search.rs
+++ src/search/search.rs
@@ -271,6 +271,12 @@ fn search<Node: NodeType>(
     let raw_eval = pos.eval();
     let corr = thread.history.corr(pos.board());
     let static_eval = adjust_eval(raw_eval, corr);
+    
+    let eval = if let Some(entry) = tt_entry && !entry.score().is_mate() && entry.flag().bounds_match(entry.score(), static_eval, static_eval) {
+        entry.score()
+    } else {
+        static_eval
+    };
 
     let improving = {
         let prev2 = ply.wrapping_sub(2);
@@ -293,8 +299,8 @@ fn search<Node: NodeType>(
     so high that even a pessimistic estimate is still above beta, we can
     be reasonably confident that a further search will also fail high.
     */
-    if !Node::PV && depth <= 8 && static_eval - Params::rfp_margin(depth, improving) >= beta {
-        return static_eval;
+    if !Node::PV && depth <= 8 && eval - Params::rfp_margin(depth, improving) >= beta {
+        return eval;
     }
 
     /*
@@ -322,7 +328,7 @@ fn search<Node: NodeType>(
         && depth >= 4
         && thread.nmr_ply != Some(ply)
         && thread.stack[ply - 1].mv.is_some()
-        && static_eval >= beta + Params::nmr_margin()
+        && eval >= beta + Params::nmr_margin()
     {
         let r = Params::nmr_reduction(depth);
         pos.make_null_move();
